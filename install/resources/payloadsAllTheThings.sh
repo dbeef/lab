87f0dfae713573aadc6090c6c6094f358b2e7ae4
Daniel 'dbeef' Zalega <daniel.zalega@gmail.com>
2024-01-13 21:18:14 +0100

    Removed 'resources' scripts as links are now in Obsidian notes

--- install/resources/payloadsAllTheThings.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source common.sh
-
-cd $RESOURCES_DIR
-git clone https://github.com/swisskyrepo/PayloadsAllTheThings
--- install/resources/seclists.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source common.sh
-
-cd $RESOURCES_DIR
-git clone https://github.com/danielmiessler/SecLists.git
